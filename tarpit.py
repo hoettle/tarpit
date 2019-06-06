3519df9534b63b9ed4a4b8f77f01c4c1bf273f76
Howard Oettle <howard@mallidax.net>
2019-06-06 14:23:53 +1000

    Refactored the verbosity down into the handlers in order for them to give more details for debugging with connects / disconnects

--- tarpit.py
+++ tarpit.py
@@ -10,22 +10,35 @@ import argparse
 import asyncio
 import random
 
+verbosity = 0
 
 async def ssh_handler(_reader, writer):
+    if (verbosity >= 1):
+        addr = writer.get_extra_info('peername')
+        print(f"SSH connected: {addr!r}")
+
     try:
         while True:
             await asyncio.sleep(10)
             writer.write(b'%x\r\n' % random.randint(0, 2**32))
             await writer.drain()
     except ConnectionResetError:
-        pass
+        if (verbosity >= 1):
+            print(f"SSH disconnected: {addr!r}")
 
 async def ssh_tarpit(port):
+    if(verbosity >= 1):
+      print(f"SSH tarpit enabled on port {port}")
+
     server = await asyncio.start_server(ssh_handler, '0.0.0.0', port)
     async with server:
         await server.serve_forever()
 
 async def http_handler(_reader, writer):
+    if (verbosity >= 1):
+        addr = writer.get_extra_info('peername')
+        print(f"HTTP connected: {addr!r}")
+
     writer.write(b'HTTP/1.1 200 OK\r\n')
     try:
         while True:
@@ -35,25 +48,36 @@ async def http_handler(_reader, writer):
             writer.write(b'X-%x: %x\r\n' % (header, value))
             await writer.drain()
     except ConnectionResetError:
-        pass
+        if (verbosity >= 1):
+            print(f"HTTP disconnected: {addr!r}")
 
 async def http_tarpit(port):
+    if(verbosity >= 1):
+      print(f"HTTP tarpit enabled on port {port}")
+
     server = await asyncio.start_server(http_handler, '0.0.0.0', port)
     async with server:
         await server.serve_forever()
 
 async def smtp_handler(_reader, writer):
-    # Fake up our header
+    if (verbosity >= 1):
+        addr = writer.get_extra_info('peername')
+        print(f"SMTP connected: {addr!r}")
     writer.write(b'220-%x\r\n' % random.randint(0,2**32))
+
     try:
         while True:
             await asyncio.sleep(10)
             writer.write(b'220-%x\r\n' % random.randint(0, 2**32))
             await writer.drain()
     except ConnectionResetError:
-        pass
+        if (verbosity >= 1):
+            print(f"SMTP disconnected: {addr!r}")
 
 async def smtp_tarpit(port):
+    if(verbosity >= 1):
+      print(f"SMTP tarpit enabled on port {port}")
+
     server = await asyncio.start_server(smtp_handler, '0.0.0.0', port, limit=1024)
     async with server:
         await server.serve_forever()
@@ -61,7 +85,7 @@ async def smtp_tarpit(port):
 def parse_args():
     parser = argparse.ArgumentParser(description='Tarpit for various services', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--verbose', '-v',  action='count', default=0)
+    parser.add_argument('--verbose', '-v',  action='count', default=verbosity)
 
     parser.add_argument('mode',             action='store', nargs='*', default='all', choices=['ssh','http','smtp', 'all'], help='enable the specified tarpit(s)')
 
@@ -81,29 +105,23 @@ def main():
     tasks = list()
     args = parse_args()
 
+    global verbosity
+    verbosity = args.verbose
+
     if('ssh' in args.mode or 'all' in args.mode):
-        if(args.verbose >= 1):
-           print("SSH tarpit enabled")
-        if(args.verbose >= 2):
-           print("SSH listening on port %d" % args.ssh_port)
-        tasks.append(ssh_tarpit(args.ssh_port))
+       tasks.append(ssh_tarpit(args.ssh_port))
 
     if('http' in args.mode or 'all' in args.mode):
-        if(args.verbose >= 1):
-           print("HTTP tarpit enabled")
-        if(args.verbose >= 2):
-           print("HTTP listening on port %d" % args.http_port)
         tasks.append(http_tarpit(args.http_port))
 
     if('smtp' in args.mode or 'all' in args.mode):
-        if(args.verbose >= 1):
-           print("SMTP tarpit enabled")
-        if(args.verbose >= 2):
-           print("SMTP listening on port %d" % args.smtp_port)
         tasks.append(smtp_tarpit(args.smtp_port))
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait(tasks))
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait(tasks))
+    except KeyboardInterrupt:
+        print('Terminated')
 
 
 
